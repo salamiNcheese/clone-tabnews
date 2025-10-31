eaea3c899a94d5bb704afb23eb9bb5d2fd945c9d
Salami <69803726+salamiNcheese@users.noreply.github.com>
2025-10-31 19:39:18 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Jéssica, eu te amo. Quero compartilhar muitos momentos ao seu lado! S2 </h1>;
+    return <h1>Jéssica, eu te amo. Quero compartilhar muitos momentos ao seu lado! S2 *beijos* LA PASSION! </h1>;
 }
 
 export default Home;
