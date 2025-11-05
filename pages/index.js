9cb87f7bbdd2c490b8df29f45dab8766fb79207d
Salami <69803726+salamiNcheese@users.noreply.github.com>
2025-11-05 18:21:23 +0000

    adiciona scripts `lint:check` e `lint:fix`´

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Jéssica, eu te amo. Quero compartilhar muitos momentos ao seu lado! S2 *beijos* LA PASSION! </h1>;
+function Home() {
+  return (
+    <h1>
+      Jéssica, eu te amo. Quero compartilhar muitos momentos ao seu lado! S2
+      *beijos* LA PASSION!{" "}
+    </h1>
+  );
 }
 
 export default Home;
