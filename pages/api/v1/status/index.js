83b6111b0b641518641e8fdae447f97cd836f511
Salami <69803726+salamiNcheese@users.noreply.github.com>
2026-02-06 23:27:30 +0000

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,13 +3,18 @@ import database from "infra/database.js"
 async function status(request, response){
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query("SHOW server_version();");
+  const databaseVersionResult = await database.query("SHOW server_version;");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
   const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  //"SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
   const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
